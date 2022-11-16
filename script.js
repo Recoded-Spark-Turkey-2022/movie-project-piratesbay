fab3d07ce9fecb894fe1636e2fa967c9dfe9e3ce
Miray Sonmez <sonmezmiray@gmail.com>
2022-11-16 13:33:30 +0300

    Actors Page

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.querySelector(".container");
+const actorsBtn=document.getElementById("actors")
 
 // Don't touch this function please
 const autorun = async () => {
   const movies = await fetchMovies();
   renderMovies(movies.results);
+  // console.log(movies.results)
 };
 
 // Don't touch this function please
@@ -78,4 +80,160 @@ const renderMovie = (movie) => {
     </div>`;
 };
 
+//Actor Section--------------------------------------------------------------
+const actorrun= async () => {
+  const actors= await fetchActors();
+  console.log(actors)
+  renderActors(actors)
+}
+
+actors.addEventListener("click", actorrun)
+
+const actorDetails = async (actor) => {
+  const actorRes = await fetchActor(actor.id);
+  console.log(actorRes)
+  renderActor(actorRes);
+};
+
+// This function is to fetch actors.
+const fetchActors = async () => {
+  const url = constructUrl(`person/popular`);
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data.results[0][3]);
+  return data.results;
+};
+// fetchActors()
+
+// This function is to fetch actor.
+const fetchActor = async (person_id) => {
+  const url = constructUrl(`person/${person_id}`);
+  const res = await fetch(url);
+  const data = await res.json();
+  return data
+};
+// fetchActor()
+
+//This function is to fetch single actor related movies(known_for)
+const actorMovieCredits = async (person_id) => {
+  const url = constructUrl(`person/${person_id}/movie_credits`)
+  const res = await fetch(url)
+  const data = await res.json()
+  const dataRes = data['cast']
+  const knownFor = document.getElementById("knownFor")
+  for (let i = 0; i < 5; i++) {
+    let imagePath = "/no_image.jpg";
+    if (dataRes[i].backdrop_path !== null) {
+      imagePath = BACKDROP_BASE_URL + dataRes[i].backdrop_path;
+      const movieCard = document.createElement("div");
+      movieCard.innerHTML = `
+        <img src="${imagePath}" alt="${dataRes[i].title
+        } poster  ">
+        <div class=" text-center">
+        <h5>${dataRes[i].title}</h5>
+        <span> ratings: ${dataRes[i].vote_average}/10</span>
+  </div>`;
+      
+      knownFor.appendChild(movieCard)
+
+      movieCard.addEventListener("click", () => {
+        movieDetails(dataRes[i]);
+      });
+    }
+
+  }
+
+};
+
+
+const renderActors = (actors) => {
+  CONTAINER.innerHTML = "";
+  const actorsContainer = document.createElement("div")
+
+  actors.map((actor) => {
+    const actorDiv = document.createElement("div");
+    actorDiv.innerHTML = `
+        <img src="${PROFILE_BASE_URL + actor.profile_path}" alt="${
+      actor.name
+    } poster">
+        <h3>${actor.name}</h3>`;
+
+    actorDiv.addEventListener("click", () => {
+      actorDetails(actor);
+    });
+    
+    actorsContainer.appendChild(actorDiv);
+    CONTAINER.appendChild(actorsContainer)
+  });
+};
+
+const renderActor = (actor) => {
+  CONTAINER.innerHTML = "";
+  CONTAINER.innerHTML = `
+    <div class="row">
+        <div class="col-md-4">
+             <img id="actor-backdrop" src=${
+               PROFILE_BASE_URL + actor.profile_path
+             }>
+        </div>
+        <div class="col-md-8">
+            <h2 id="actor-name">${actor.name}</h2>
+
+            <p id="actor-gender"><b>Gender:</b> ${
+              actor.gender == 1 ? "famale" : "male"
+            }</p>
+            <p id="actor-popularity"><b>Popularity:</b> ${actor.popularity}</p>
+            <p id="actor-birthday"><b>Birthday:</b> ${actor.birthday}</p>
+            <p id="actor-deathday"><b>Deathday:</b> ${actor.deathday}</p>
+
+            <h4 id="actor-bio">${actor.biography}</h4>
+        </div>
+        <div>
+          <h4  id="moviesBy" style="padding:1rem;"> Related Movies:</h4> 
+          <div class="row justify-content-center" id="knownFor"></div>
+        </div>
+    </div>`;
+
+    if (actor.deathday === null) {
+      document.getElementById("actor-deathday").remove()
+    }
+    actorMovieCredits(actor.person_id)
+};
+
+//------------------------------------------------------------------------------------
+
 document.addEventListener("DOMContentLoaded", autorun);
+
+const dropDownButton = document.querySelector(".dropbtn")
+const dropDownContent = document.querySelector(".dropdown-content")
+dropDownButton.addEventListener("click", () => {
+  dropDownContent.classList.toggle("show")
+})
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    if (dropDownContent.classList.contains('show')) {
+      dropDownContent.classList.remove('show');
+    }
+  }
+}
+
+//Shortcut for moving the cursor to the search box
+const searchInput = document.getElementById("search-input");
+
+let isKeyPressed = { 
+  'a': false, // ASCII code for 'a'
+  'q': false, // ASCII code for 'k'
+   // ... Other key codes you want to track
+};
+
+document.onkeydown = (e) => {
+  isKeyPressed[e.key] = true; 
+  if (e.ctrlKey && isKeyPressed["q"]) {
+    searchInput.focus();
+  }
+};
+
+document.onkeyup = (e) => {
+  isKeyPressed[e.key] = false;
+};
